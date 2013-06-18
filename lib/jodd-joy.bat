e3bbd125091e4f402f8524035f07894c5ea8e190
Igor <igor@jodd.org>
2013-06-18 23:40:53 +0200

    new jodd, new tomcat

--- lib/jodd-joy.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-copy d:\Projects\java\oblac\jodd\jodd-joy\build\libs\jodd-joy-3.4.3-SNAPSHOT.jar jodd
--- lib/jodd.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-copy d:\Projects\java\oblac\jodd\distribution\build\lib\jodd-3.4.3-SNAPSHOT.jar jodd
